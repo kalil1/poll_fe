b901608a833645f4919358a62137a1e941151feb
kalil1 <wkalil77@gmail.com>
2025-06-03 19:38:03 -0400

    just doing stuff at this point

--- src/components/polls.js
+++ src/components/polls.js
@@ -23,10 +23,14 @@ const VotingPoll = () => {
       <ul>
         {(pollData).map((poll) => (
           <li>
-            {poll.id}
-            <ul>
-              {poll.title} : {poll.completed? "true" : "false"}
-            </ul>
+            <div className="card" width="18rem;">
+              <img className="card-img-top" src="..." alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">{poll.title}</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
